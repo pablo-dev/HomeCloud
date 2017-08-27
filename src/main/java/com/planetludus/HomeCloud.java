2ef806b171996ebbb583b46b11dbfef4238b7b04
pablo-dev <pablo.carnero.linan@gmail.com>
2017-08-27 19:49:41 +0200

    Tested with android client

--- src/main/java/com/planetludus/HomeCloud.java
+++ src/main/java/com/planetludus/HomeCloud.java
@@ -19,9 +19,11 @@ public class HomeCloud {
 
     private final static int DEFAULT_PORT = 3999;
     private final static String DEFAULT_STORAGE_PATH = "C:\\Users\\pablo\\Temp";
+    private final static int DEFAULT_BUFFER_SIZE = 1024;
     
     private static int port = DEFAULT_PORT;
     private static String storagePath = DEFAULT_STORAGE_PATH;
+    private static int bufferSize = DEFAULT_BUFFER_SIZE;
     
     /**
      * Read the input arguments
@@ -39,6 +41,9 @@ public class HomeCloud {
         
         Option pathOption = new Option("d", "directory", true, "storage directory");
         options.addOption(pathOption);
+
+        Option bufferOption = new Option("b", "bufferSize", true, "buffer size");
+        options.addOption(bufferOption);
         
         // TODO: add user and password option for authentication
         
@@ -59,6 +64,15 @@ public class HomeCloud {
             }
             
             storagePath = cmd.getOptionValue("d", DEFAULT_STORAGE_PATH);
+
+            String bufferSizeStr = cmd.getOptionValue("b");
+            if (bufferSizeStr != null) {
+                try {
+                    bufferSize = Integer.parseInt(bufferSizeStr);
+                } catch (NumberFormatException e) {
+                    throw new ParseException("Integer value required for bufferSize: " + bufferSizeStr);
+                }
+            }
             
         } catch (ParseException ex) {
             logger.error("Invalid arguments: " + ex.getMessage());
@@ -85,8 +99,7 @@ public class HomeCloud {
                 PropManager propManager = new PropManager(storagePath);
                 ServerSocket serverSocket = new ServerSocket(port)) {
             
-            while (true)
-            {
+            while (true) {
                 logger.info("awaiting for connection in port " + port + " ...");
                 try (
                         Socket clientSocket = serverSocket.accept();
@@ -95,26 +108,29 @@ public class HomeCloud {
                         OutputStream out = clientSocket.getOutputStream();
                         DataOutputStream serverData = new DataOutputStream(out)
                     ) {
-                    
+
                     String clientId = clientData.readUTF();
-                    
+
                     logger.info("connection established with " + clientId);
-                    
+
+                    logger.info("sending the buffer size " + bufferSize);
+                    serverData.writeInt(bufferSize);
+
                     logger.info("sending the last sync date " + propManager.getLastSync(clientId));
                     serverData.writeUTF(propManager.getLastSync(clientId));
 
                     int fileNumbers = clientData.readInt();
                     logger.info("ready to receive " + fileNumbers + " files");
-                    
+
                     for (int i = 0; i < fileNumbers; i++) {
-                        
+
                         String fileName = clientData.readUTF();
                         logger.info("receiving the file number " + i + " with name: " + fileName);
 
-                        output = new FileOutputStream(storagePath + "\\" + fileName);
+                        output = new FileOutputStream(propManager.getFolder(clientId) + "\\" + fileName);
 
-                        long size = clientData.readLong();     
-                        byte[] buffer = new byte[1024];
+                        long size = clientData.readLong();
+                        byte[] buffer = new byte[bufferSize];
                         while (size > 0 && (bytesRead = clientData.read(buffer, 0, (int) Math.min(buffer.length, size))) != -1) {
                             output.write(buffer, 0, bytesRead);
                             size -= bytesRead;
@@ -122,20 +138,21 @@ public class HomeCloud {
                         }
 
                         logger.info(fileName + " recived");
-                    
+
                     }
-                    
+
                     propManager.setLastSync(clientId);
-                    
+                } catch (IOException ex) {
+                    logger.error("Failed transaction: Input/output Error", ex);
                 } finally {
                     if (output != null) output.close();
                 }
             }
-            
+
         } catch (IOException ex) {
             logger.error("Input/output Error", ex);
         } catch (ConfigurationException ex) {
-            logger.error("Error reading the config file. Probably the format is wrong", ex);
+            logger.error("Failed transaction: Error reading the config file. Probably the format is wrong", ex);
         }
     }
 
